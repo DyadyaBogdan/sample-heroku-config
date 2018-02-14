462d5e7fd8b8ef5a99a7de56eef094e7daa9f2c8
DyadyaBogdan <31926180+DyadyaBogdan@users.noreply.github.com>
2018-02-14 17:26:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from time import sleep
+
+import requests
+from handler import handle_message
 
 while True:
-    print("Tick!")
-    sleep(10)
-    print("Tack!")
-    sleep(10)
+    res = request.get("https://api.telegram.org/bot504418727:AAFpnAZ7uJ7ZUMJzgZ5apcC-aOJELjWFlQY/getUpdates")
+    d = res.json()
+
+    for elem in d["result"]:
+        text = elem["message"]["text"]
+        ans = handle_message(text)
+
+        chat_id = elem["message"]["chat"]["id"]
+
+        requests.post("https://api.telegram.org/bot504418727:AAFpnAZ7uJ7ZUMJzgZ5apcC-aOJELjWFlQY/sendMessage", params={ "chat_id": chat_id, "text": text } )
