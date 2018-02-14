3e05e265d38f03595a2c2ac30f707f7b41a40c46
DyadyaBogdan <31926180+DyadyaBogdan@users.noreply.github.com>
2018-02-14 17:10:05 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,6 +1,5 @@
 smiles = {'fight': "(ᕗ ͠°  ਊ ͠° )ᕗ", 'angry': "୧( ಠ Д ಠ )୨", 'ugly': "(   ͡°╭╮ʖ   ͡°)", 'dead': "ᕦ༼ ✖ ਊ ✖ ༽ᕤ", 'scared': "(∩╹□╹∩)"}
 smiles_pop = {'fight': 0, 'angry': 0, 'ugly': 0, 'dead': 0, 'scared': 0}
-print(list(smiles_pop.keys()))
 
 
 def handle_message(dir_1, name, dir_2):
@@ -8,14 +7,19 @@ def handle_message(dir_1, name, dir_2):
         dir_2[name] += 1
         return dir_1[name]
     elif name == "Most popular":
-        print(list(dir_2.keys())[list(dir_2.values()).index(max(list(dir_2.values())))])
+        max_used = -1
+        for i in dir_2:
+            if dir_2[i] > max_used:
+                max_used = dir_2[i]
+                smile = i
+        return smile
     else:
         return "Smile is not found"
 
 
 if __name__ == "__main__":
     print("Smiles list:")
-    print(*list(smiles.keys()))
+    print(*smiles.keys())
     while True:
         smile_name = input("Enter necessary smile name: ")
         ans = handle_message(smiles, smile_name, smiles_pop)
